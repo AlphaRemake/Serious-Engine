7b494b684ccec0e556ca80e457f69317aeb7abda
Zdzichu <148331438+GrlPwr-Zdzichu@users.noreply.github.com>
2023-11-08 21:53:01 +0100

    Add header files for Cyberman enemy.

---
+++ Sources/Models/Enemies/Cyberman/Holder.h
@@ -0,0 +1,16 @@
+// Animation names
+#define HOLDER_ANIM_DEFAULT 0
+#define HOLDER_ANIM_RUN 1
+
+// Color names
+
+// Patch names
+
+// Names of collision boxes
+#define HOLDER_COLLISION_BOX_PART_NAME 0
+#define HOLDER_COLLISION_BOX_DEATH 1
+
+// Attaching position names
+#define HOLDER_ATTACHMENT_CYBERMAN 0
+
+// Sound names
